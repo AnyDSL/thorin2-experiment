1f0718331c3b77496d4ae0d1606d7a15a0797e68
Roland Leißa <leissa@cs.uni-saarland.de>
2016-10-16 21:35:59 +0200

    putting whitespace at work

--- main.cpp
+++ main.cpp
@@ -5,12 +5,16 @@ using namespace thorin;
 int main()  {
     World w;
     auto Nat = w.assume(w.star(), "Nat");
+
     // λT:*.λx:T.x
     auto poly_id = w.lambda(w.star(), w.lambda(w.var(w.star(), 1), w.var(w.var(w.star(), 2), 1)));
     poly_id->dump();
     poly_id->type()->dump();
+
     // λx:Nat.x
     auto int_id = w.app(poly_id, Nat);
     int_id->dump();
+
+    // 42
     w.app(int_id, w.assume(Nat, "42"))->dump();
 }
