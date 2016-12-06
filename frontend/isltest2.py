6ca669a6eee2d98feb756dd9eeede9d4fb3d6cc9
Markus Bauer <s9mkbaue@stud.uni-saarland.de>
2016-12-06 15:22:21 +0100

    Demos

---
+++ frontend/isltest2.py
@@ -0,0 +1,54 @@
+import islpy as isl
+
+ctx = isl.DEFAULT_CONTEXT
+
+# check for transitive closure
+'''
+define test = lambda rec f (x,n:Nat,Nat):Nat. if x < n then 1 + f (x+1, n) else n;
+Expected: [[x,n], [result]], {x,n,r}, {x,n,r | r = n}
+
+{x1, n1, r1}   =>   {x2, n2, r2, x1, n1, r2 | x2<n2  & n1=n2 & x1=x2+1 & r2=r1}
+                  U {x2, n2, r2, x1, n1, r2 | x2>=n2 & r2=n2}
+
+'''
+
+space = isl.Space.create_from_names(ctx, in_=["x", "n", "r"], out=['x2', 'n2', 'r2'])
+m = m2 = isl.Map.universe(space)
+m = m.add_constraint(isl.Constraint.eq_from_names(space, {'n2': 1, 'n': -1}))
+m = m.add_constraint(isl.Constraint.eq_from_names(space, {'r2': 1, 'r': -1, 1: -1}))
+m = m.add_constraint(isl.Constraint.eq_from_names(space, {'x2': 1, 'x': -1, 1: 1}))
+m = m.add_constraint(isl.Constraint.ineq_from_names(space, {'n2': 1, 1: -1, 'x2': -1}))
+
+m2 = m2.add_constraint(isl.Constraint.eq_from_names(space, {'r2': 1, 'n2': -1}))
+m2 = m2.add_constraint(isl.Constraint.ineq_from_names(space, {'x2': 1, 'n2': -1}))
+bs2 = isl.BasicSet.universe(space.range())
+bs2 = bs2.add_constraint(isl.Constraint.eq_from_names(space.range(), {'r2': 1, 'n2': -1}))
+bs2 = bs2.add_constraint(isl.Constraint.ineq_from_names(space.range(), {'x2': 1, 'n2': -1}))
+
+print '  ', m
+print 'U ', m2
+m_union = m.union(m2)
+print '=>', m_union
+m_closure, exact = m_union.transitive_closure()
+print 'Closure:', m_closure, ' exact =',exact
+m_closure = m_closure.coalesce().detect_equalities().remove_redundancies()
+print 'Closure:', m_closure, ' exact =',exact
+# apply to all non-recursive cases
+m_result = m2.range().apply(m_closure)
+print 'Result: ', m_result
+
+print '\n\n'
+m_closure, exact = m.transitive_closure()
+print 'Closure:', m_closure, ' exact =',exact
+m_closure = m_closure.coalesce().detect_equalities().remove_redundancies()
+print 'Closure:', m_closure, ' exact =',exact
+m_result = bs2.apply(m_closure).union(bs2).coalesce()
+print 'Result: ', m_result
+
+
+'''
+[x, n, r]     -> [x2, n2, r2] : n2 > x2
+[x, n, r]     -> [x2, n2, r2 = n2] : n2 <= x2
+[x, n, r]     -> [x2, n2 = n, r2 = r] : x2 < n and x2 < x
+[x, n, r = n] -> [x2, n2 = n, r2 = n] : n <= x2 < x
+'''
