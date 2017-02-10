d25693c459b6ded942a719090f14c988571c9c1e
Roland Leißa <leissa@cs.uni-saarland.de>
2017-02-10 02:28:57 +0100

    whitespaces

--- thorin/qualifier.h
+++ thorin/qualifier.h
@@ -12,38 +12,38 @@ namespace thorin {
  * Where Linear is the largest element in the partial orders </<= and Unlimited the smallest.
  */
 enum class Qualifier {
-  Unlimited,
-  Relevant = 1 << 0,
-  Affine   = 1 << 1,
-  Linear = Affine | Relevant,
+    Unlimited,
+    Relevant = 1 << 0,
+    Affine   = 1 << 1,
+    Linear = Affine | Relevant,
 };
 
 /// Linear is the largest, Unlimited the smallest.
 inline bool operator<(Qualifier lhs, Qualifier rhs) {
-  if (lhs == rhs) return false;
-  if (lhs == Qualifier::Unlimited) return true;
-  if (rhs == Qualifier::Linear) return true;
-  return false;
+    if (lhs == rhs) return false;
+    if (lhs == Qualifier::Unlimited) return true;
+    if (rhs == Qualifier::Linear) return true;
+    return false;
 }
 
 inline bool operator<=(Qualifier lhs, Qualifier rhs) {
-  return lhs == rhs || lhs < rhs;
+    return lhs == rhs || lhs < rhs;
 }
 
 constexpr const char* qualifier_cstr(Qualifier q) {
-  return q == Qualifier::Unlimited ? "" :
-    q == Qualifier::Relevant ? "ᴿ" :
-    q == Qualifier::Affine ? "ᴬ" :
-    "ᴸ";
+    return q == Qualifier::Unlimited ? "" :
+        q == Qualifier::Relevant ? "ᴿ" :
+        q == Qualifier::Affine ? "ᴬ" :
+        "ᴸ";
 }
 
 inline std::ostream& operator<<(std::ostream& ostream, const Qualifier q) {
-  return ostream << qualifier_cstr(q);
+    return ostream << qualifier_cstr(q);
 }
 
 /// Also known as the least upper bound.
 inline Qualifier join(Qualifier lhs, Qualifier rhs) {
-  return Qualifier(static_cast<int>(lhs) | static_cast<int>(rhs));
+    return Qualifier(static_cast<int>(lhs) | static_cast<int>(rhs));
 }
 
 /// Also known as the greatest lower bound.
