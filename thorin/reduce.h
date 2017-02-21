1066f65356a679b112280667da2eafa9349687f1
Roland Leißa <leissa@cs.uni-saarland.de>
2017-02-22 00:16:34 +0100

    typo

--- thorin/reduce.h
+++ thorin/reduce.h
@@ -12,7 +12,7 @@ const Def* reduce(const Def* def, Defs args, size_t index = 0);
  * Reduces @p def with @p args using @p index to indicate the Var.
  * First, all structural stuff in @p def is reduced.
  * Then, the hook @p f is called with the result of the first reduction run.
- * Finally, a fixed-pointer iteration is performed to reduce the rest.
+ * Finally, a fixed-point iteration is performed to reduce the rest.
  */
 const Def* reduce(const Def* def, Defs args, std::function<void(const Def*)> f, size_t index = 0);
 
