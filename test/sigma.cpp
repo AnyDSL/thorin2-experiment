c4cf544f0cc84db05151e4d88454cdeff0f9bd0b
Roland Leißa <leissa@cs.uni-saarland.de>
2017-02-22 23:35:04 +0100

    sorting tests

---
+++ test/sigma.cpp
@@ -0,0 +1,52 @@
+#include "gtest/gtest.h"
+
+#include "thorin/world.h"
+
+using namespace thorin;
+
+TEST(Sigma, Assign) {
+    World w;
+    auto sig = w.sigma({w.star(), w.var(w.star(), 0)})->as<Sigma>();
+    ASSERT_TRUE(sig->assignable({w.type_nat(), w.val_nat(42)}));
+    ASSERT_FALSE(sig->assignable({w.type_nat(), w.val_bool_bot()}));
+}
+
+TEST(Sigma, ExtractAndSingleton) {
+    World w;
+    auto n23 = w.val_nat(23);
+    auto n42 = w.val_nat(42);
+
+    auto fst = w.lambda({w.type_nat(), w.type_nat()}, w.var(w.type_nat(), 1));
+    auto snd = w.lambda({w.type_nat(), w.type_nat()}, w.var(w.type_nat(), 0));
+    ASSERT_EQ(w.app(fst, {n23, n42}), w.val_nat(23));
+    ASSERT_EQ(w.app(snd, {n23, n42}), w.val_nat(42));
+
+    auto poly = w.axiom(w.pi(w.star(), w.star()), {"Poly"});
+    auto sigma = w.sigma({w.star(), w.app(poly, w.var(w.star(), 0))}, {"sig"});
+    auto sigma_val = w.axiom(sigma,{"val"});
+    auto fst_sigma = w.extract(sigma_val, 0_s);
+    ASSERT_EQ(fst_sigma->type(), w.star());
+    auto snd_sigma = w.extract(sigma_val, 1);
+    snd_sigma->type()->dump();
+    auto single_sigma = w.singleton(sigma_val);
+    std::cout << single_sigma << ": " << single_sigma->type() << std::endl;
+    auto single_pi = w.singleton(w.axiom(w.pi(w.star(), w.star()), {"pival"}));
+    std::cout << single_pi << ": " << single_pi->type() << std::endl;
+}
+
+TEST(Sigma, Unit) {
+    World w;
+
+    auto unit = w.unit();
+    auto tuple0 = w.tuple0();
+    ASSERT_EQ(tuple0->type(), unit);
+
+    auto lam = w.lambda(unit, tuple0);
+    ASSERT_EQ(lam->domain(), unit);
+    ASSERT_TRUE(lam->domains().size() == 0);
+    ASSERT_EQ(lam->type()->body(), unit);
+    auto pi = w.pi(Defs({}), unit);
+    ASSERT_EQ(pi, lam->type());
+    auto apped = w.app(lam, tuple0);
+    ASSERT_EQ(tuple0, apped);
+}
