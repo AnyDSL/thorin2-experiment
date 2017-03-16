893852fc53e35bb17f7b7d24f94fb17a1e51398f
Klaas Boesche <boesche@cs.uni-saarland.de>
2017-03-16 19:42:30 +0100

    Remove broken test case

--- test/nominal.cpp
+++ test/nominal.cpp
@@ -115,35 +115,6 @@ TEST(Nominal, PolymorphicList) {
     ASSERT_EQ(apped->op(1)->op(0), nat);
 }
 
-TEST(Nominal, PolymorphicListNoOuterNominal) {
-    World w;
-    auto nat = w.type_nat();
-    auto star = w.star();
-
-    auto nil = w.sigma_type(0, {"nil"});
-    ASSERT_TRUE(nil->is_closed());
-    print_value_type(nil);
-
-    auto cons = w.sigma_type(2, {"cons"});
-
-    auto cons_or_nil = w.variant({cons, nil}, star);
-    auto list = w.lambda(star, cons_or_nil, {"list"});
-    ASSERT_TRUE(list->is_closed());
-
-    cons->set(0, w.var(star, 0));
-    cons->set(1, w.app(list, w.var(star, 1)));
-    ASSERT_TRUE(cons->is_closed());
-    ASSERT_TRUE(cons->free_vars().any_end(1));
-    ASSERT_TRUE(cons_or_nil->free_vars().any_end(1));
-    print_value_type(cons);
-    print_value_type(list);
-    auto apped = w.app(list, nat);
-    print_value_type(apped);
-    print_value_type(apped->op(0));
-    print_value_type(apped->op(1));
-    ASSERT_EQ(apped->op(1)->op(0), nat);
-}
-
 TEST(Nominal, PolymorphicListVariantNominal) {
     World w;
     auto nat = w.type_nat();
