689c6e02fbb79f1586d706156335e438da6b67ff
Klaas Boesche <boesche@cs.uni-saarland.de>
2017-06-06 17:35:19 +0200

    add file for op matcher tests

---
+++ test/matchers/ops.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+
+#include "thorin/matchers/ops.h"
+
+namespace thorin {
+
+TEST(MatchOps, Arithmetic) {
+    // World w;
+}
+
+TEST(MatchOps, Compare) {
+    // World w;
+}
+
+TEST(MatchOps, Pointer) {
+    // World w;
+}
+
