4e5406b4c3f4e12fafd87d19ccc7239f4cd3fcde
Roland Leißa <leissa@cs.uni-saarland.de>
2017-11-30 17:15:02 +0100

    wip: lexer

--- thorin/frontend/lexer.cpp
+++ thorin/frontend/lexer.cpp
@@ -7,15 +7,26 @@
 
 namespace thorin {
 
+static inline int sym(int c) { return std::isalpha(c) || c == '_'; }
+static inline int dec_nonzero(int c) { return c >= '1' && c <= '9'; }
+static inline int bin(int c) { return '0' <= c && c <= '1'; }
+static inline int oct(int c) { return '0' <= c && c <= '7'; }
+//static inline bool hex(int c) { return std::isxdigit(c) != 0; }
+static inline int eE(int c) { return c == 'e' || c == 'E'; }
+static inline int sgn(int c){ return c == '+' || c == '-'; }
+
 Lexer::Lexer(std::istream& is, const char* filename)
     : stream_(is)
     , filename_(filename)
 {
     if (!stream_)
         throw std::runtime_error("stream is bad");
+    next();
 }
 
 uint32_t Lexer::next() {
+    uint32_t result = peek_;
+
     int c = stream_.get();
 
     // see https://en.wikipedia.org/wiki/UTF-8
@@ -28,98 +39,78 @@ uint32_t Lexer::next() {
             peek_col_ = 1;
         } else if (c != std::istream::traits_type::eof())
             ++peek_col_;
-
-        return c;
+        peek_ = c;
     } else if (((c & 0b11000000) != 0) && ((~c & 0b00100000) != 0)) { // 2-bytes: 110xxxxx 10xxxxxx
     } else if (((c & 0b11100000) != 0) && ((~c & 0b00010000) != 0)) { // 3 bytes: 1110xxxx 10xxxxxx 10xxxxxx
     } else if (((c & 0b11110000) != 0) && ((~c & 0b00001000) != 0)) { // 4 bytes: 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
     } else {
     }
+
+    return result;
 }
 
 Token Lexer::lex() {
-    eat_spaces();
+    while (true) {
+        // skip whitespace
+        if (accept(std::isspace)) {
+            while (accept(std::isspace)) {}
+            continue;
+        }
 
-    auto front_line = line_;
-    auto front_col = col_;
-    auto make_loc = [&] () {
-        return Location(filename_.c_str(), front_line, front_col, line_, col_);
-    };
+        std::string str; // the token string is concatenated here
+        front_line_ = peek_line_;
+        front_col_ = peek_col_;
 
-    if (eof()) return Token(make_loc(), Token::Tag::Eof);
+        if (stream_.eof())
+            return {location(), Token::Tag::Eof};
 
-    if (accept('{')) return Token(make_loc(), Token::Tag::L_Brace);
-    if (accept('}')) return Token(make_loc(), Token::Tag::R_Brace);
-    if (accept('(')) return Token(make_loc(), Token::Tag::L_Paren);
-    if (accept(')')) return Token(make_loc(), Token::Tag::R_Paren);
-    if (accept('[')) return Token(make_loc(), Token::Tag::L_Bracket);
-    if (accept(']')) return Token(make_loc(), Token::Tag::R_Bracket);
-    if (accept('<')) return Token(make_loc(), Token::Tag::L_Angle);
-    if (accept('>')) return Token(make_loc(), Token::Tag::R_Angle);
-    if (accept(':')) return Token(make_loc(), Token::Tag::Colon);
-    if (accept(',')) return Token(make_loc(), Token::Tag::Comma);
-    if (accept('.')) return Token(make_loc(), Token::Tag::Dot);
-    if (accept(';')) return Token(make_loc(), Token::Tag::Semicolon);
-    if (accept('*')) return Token(make_loc(), Token::Tag::Star);
+        if (accept('{')) return {location(), Token::Tag::L_Brace};
+        if (accept('}')) return {location(), Token::Tag::R_Brace};
+        if (accept('(')) return {location(), Token::Tag::L_Paren};
+        if (accept(')')) return {location(), Token::Tag::R_Paren};
+        if (accept('[')) return {location(), Token::Tag::L_Bracket};
+        if (accept(']')) return {location(), Token::Tag::R_Bracket};
+        if (accept('<')) return {location(), Token::Tag::L_Angle};
+        if (accept('>')) return {location(), Token::Tag::R_Angle};
+        if (accept(':')) return {location(), Token::Tag::Colon};
+        if (accept(',')) return {location(), Token::Tag::Comma};
+        if (accept('.')) return {location(), Token::Tag::Dot};
+        if (accept(';')) return {location(), Token::Tag::Semicolon};
+        if (accept('*')) return {location(), Token::Tag::Star};
 
-    if (accept('#')) {
-        if (accept("lambda")) return Token(make_loc(), Token::Tag::Lambda);
-        if (accept("pi"))     return Token(make_loc(), Token::Tag::Pi);
-        if (accept("true"))   return Token(make_loc(), Literal(Literal::Tag::Lit_bool, Box(true)));
-        if (accept("false"))  return Token(make_loc(), Literal(Literal::Tag::Lit_bool, Box(false)));
+        if (accept('#')) {
+            if (accept("lambda")) return {location(), Token::Tag::Lambda};
+            if (accept("pi"))     return {location(), Token::Tag::Pi};
+            if (accept("true"))   return {location(), Literal(Literal::Tag::Lit_bool, Box(true))};
+            if (accept("false"))  return {location(), Literal(Literal::Tag::Lit_bool, Box(false))};
 
-        return Token(make_loc(), Token::Tag::Sharp);
+            return {location(), Token::Tag::Sharp};
+        }
+
+        // greek letters
+        if (accept(0x0003bb)) return {location(), Token::Tag::Lambda};
+        if (accept(0x0003a0)) return {location(), Token::Tag::Pi};
+        if (accept(0x01D538)) return {location(), Token::Tag::Arity_Kind};
+        if (accept(0x01D544)) return {location(), Token::Tag::Multi_Arity_Kind};
+        if (accept(0x00211A)) {
+            if (accept(0x2096))
+                return {location(), Token::Tag::Qualifier_Kind};
+            return {location(), Token::Tag::Qualifier_Type};
+        }
+
+        if (std::isdigit(peek()) || peek() == '+' || peek() == '-') {
+            auto lit = parse_literal();
+            return {location(), lit};
+        }
+
+        // identifier
+        if (accept(str, sym)) {
+            while (accept(str, sym) || accept(str, std::isdigit)) {}
+            return {location(), str};
+        }
+
+        ELOG_LOC(location(), "invalid character '{}'", (char) next());
     }
-
-    // greek letters
-    if (accept(0x0003bb)) return Token(make_loc(), Token::Tag::Lambda);
-    if (accept(0x0003a0)) return Token(make_loc(), Token::Tag::Pi);
-    if (accept(0x01D538)) return Token(make_loc(), Token::Tag::Arity_Kind);
-    if (accept(0x01D544)) return Token(make_loc(), Token::Tag::Multi_Arity_Kind);
-    if (accept(0x00211A)) {
-        if (accept(0x2096))
-            return Token(make_loc(), Token::Tag::Qualifier_Kind);
-        return Token(make_loc(), Token::Tag::Qualifier_Type);
-    }
-
-    if (std::isdigit(peek()) || peek() == '+' || peek() == '-') {
-        auto lit = parse_literal();
-        return Token(make_loc(), lit);
-    }
-
-    std::string ident;
-    auto is_identifier_char = [&] (uint32_t c) {
-        return std::isalpha(c) || c == '_' || (ident != "" && std::isdigit(c));
-    };
-    while (!eof() && is_identifier_char(peek())) {
-        ident += peek();
-        eat();
-    }
-    if (ident != "") return Token(make_loc(), ident);
-
-    std::string invalid;
-    utf8::append(peek(), std::back_inserter(invalid));
-    ELOG_LOC(make_loc(), "invalid character '{}'", invalid);
-}
-
-void Lexer::eat() {
-    if (iterator_ == std::istreambuf_iterator<char>()) {
-        eof_ = true;
-        return;
-    }
-
-    if (peek() == '\n') {
-        line_++;
-        col_ = 1;
-    } else {
-        col_++;
-    }
-
-    cur_ = utf8::unchecked::next(iterator_);
-}
-
-void Lexer::eat_spaces() {
-    while (!eof() && std::isspace(peek())) eat();
 }
 
 Literal Lexer::parse_literal() {
@@ -131,7 +122,7 @@ Literal Lexer::parse_literal() {
                (base == 16 && peek() >= 'a' && peek() <= 'f') ||
                (base == 16 && peek() >= 'A' && peek() <= 'F')) {
             lit += peek();
-            eat();
+            next();
         }
     };
 
@@ -199,7 +190,7 @@ Literal Lexer::parse_literal() {
         return Literal(Literal::Tag::Lit_untyped, u64(strtoull(lit.c_str(), nullptr, 10)));
     }
 
-    ELOG_LOC(Location(filename_.c_str(), line_, col_), "invalid literal in {}");
+    ELOG_LOC(location(), "invalid literal in {}");
 }
 
 bool Lexer::accept(uint32_t c) {
@@ -210,13 +201,12 @@ bool Lexer::accept(uint32_t c) {
     return false;
 }
 
-// TODO this method is basically broken because mathcing "ac" won't work:
-// if (accept("ab")) ...
-// if (accept("ac")) ..
+/*
 bool Lexer::accept(const std::string& str) {
     auto it = str.begin();
     while (it != str.end() && accept(*(it++))) ;
     return it == str.end() && (eof() || !(peek() == '_' || std::isalnum(peek())));
 }
+*/
 
 }
--- thorin/frontend/lexer.h
+++ thorin/frontend/lexer.h
@@ -15,18 +15,32 @@ public:
 
 private:
     void eat();
-    void eat_spaces();
     Literal parse_literal();
 
     bool accept(uint32_t);
     bool accept(const std::string&);
+    bool accept(int (*pred)(int)) {
+        if (pred(peek())) {
+            next();
+            return true;
+        }
+        return false;
+    }
+    bool accept(std::string& str, int (*pred)(int)) {
+        if (pred(peek())) {
+            str += next();
+            return true;
+        }
+        return false;
+    }
 
     uint32_t next();
-    int peek() const { return stream_.peek(); }
+    uint32_t peek() const { return peek_; }
     Location location() const { return {filename_, front_line_, front_col_, back_line_, back_col_}; }
     Location curr() const { return location().back(); }
 
     std::istream& stream_;
+    uint32_t peek_;
     const char* filename_;
     uint32_t front_line_ = 1, front_col_ = 1, back_line_ = 1, back_col_ = 1, peek_line_ = 1, peek_col_ = 1;
 };
