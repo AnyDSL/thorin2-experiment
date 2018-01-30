b6725412010b93594d808a3494790c1fee38f480
Roland Leißa <leissa@cs.uni-saarland.de>
2018-01-30 21:36:03 +0100

    cn test case: use core world

--- test/cn.cpp
+++ test/cn.cpp
@@ -1,23 +1,25 @@
 #include "gtest/gtest.h"
 
-#include "thorin/world.h"
+#include "thorin/core/world.h"
 #include "thorin/frontend/parser.h"
 
-namespace thorin {
+namespace thorin::core {
 
 TEST(Cn, Simpel) {
     World w;
     auto C = w.cn_type(w.unit());
-    auto k = w.cn(parse(w, "cn[nat, cn nat]")->as<CnType>());
-    //auto x = k->param(0, {"x"});
+    auto k = w.cn(parse(w, "cn[int {32s64: nat}, cn int {32s64: nat}]")->as<CnType>());
+    auto x = k->param(0, {"x"});
     auto r = k->param(1, {"r"});
     auto t = w.cn(C, {"t"});
     auto f = w.cn(C, {"f"});
-    auto n = w.cn(parse(w, "cn[nat]")->as<CnType>());
-    k->br(w.lit_true(), t, f);
-    t->jump(n, w.lit_nat_16());
-    f->jump(n, w.lit_nat_32());
+    auto n = w.cn(parse(w, "cn[int {32s64: nat}]")->as<CnType>());
+    k->br(w.op<ICmp::ugt>(x, w.lit_i(0_u32)), t, f);
+    t->jump(n, w.lit_i(23_u32));
+    f->jump(n, w.lit_i(42_u32));
     n->jump(r, n->param());
+
+    w.make_external(k);
 }
 
 }
