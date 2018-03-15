78a27842f4111fba9ce4315c169bf5bea4264738
Roland Leißa <leissa@cs.uni-saarland.de>
2018-03-15 17:20:34 +0100

    remove unused code

--- thorin/transform/reduce.h
+++ thorin/transform/reduce.h
@@ -12,9 +12,6 @@ inline const Def* reduce(const Def* def, const Def* arg, size_t index = 0) { ret
 /// Flattens the use of Var @c 0 and Extract%s in @p body to directly use @p args instead.
 const Def* flatten(const Def* body, Defs args);
 
-/// Unflattens the use of Vars @c 0, ..., n and uses Extract%s in @p body instead.
-const Def* unflatten(const Def* body, const Def* arg);
-
 /// Adds @p shift to all free variables in @p def.
 const Def* shift_free_vars(const Def* def, int64_t shift);
 }
